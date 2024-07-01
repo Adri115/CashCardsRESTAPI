0938341989b4ce6ecb0d634297dcd427ae3fabf3
Adrian <adriangomezp15@gmail.com>
2024-07-01 08:37:39 -0600

    Created Controller,Repository and Tests for JsonFormat and GET/POST endpoints using H2 db.

---
+++ cashCard/src/main/java/com/cashCard/CashCard.java
@@ -0,0 +1,7 @@
+package com.cashCard;
+
+import org.springframework.data.annotation.Id;
+
+public record CashCard(@Id Long id,Double amount) {
+
+}
---
+++ cashCard/src/main/java/com/cashCard/repository/CashCardRepository.java
@@ -0,0 +1,8 @@
+package com.cashCard.repository;
+
+import com.cashCard.CashCard;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CashCardRepository extends CrudRepository<CashCard, Long> {
+
+}
