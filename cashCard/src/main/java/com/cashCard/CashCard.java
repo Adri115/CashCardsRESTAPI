191e70bc6ad0f8e369f2fc851e3769687332fa7f
Adrian <adriangomezp15@gmail.com>
2024-07-01 18:23:25 -0600

    Added SpringBootRestAPIProject, ignored cashcards

--- cashCard/src/main/java/com/cashCard/CashCard.java
+++
@@ -1,7 +0,0 @@
-package com.cashCard;
-
-import org.springframework.data.annotation.Id;
-
-public record CashCard(@Id Long id,Double amount) {
-
-}
--- cashCard/src/main/java/com/cashCard/repository/CashCardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cashCard.repository;
-
-import com.cashCard.CashCard;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CashCardRepository extends CrudRepository<CashCard, Long> {
-
-}
